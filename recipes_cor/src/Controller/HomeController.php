45abbce6225fce770c1f9137250d5f8edaeb3fad
BIVEGUE Isabelle <isabelle.biveguengoa.etu@univ-lille.fr>
2023-12-01 12:51:22 +0100

    Nettoyage

--- recipes_cor/src/Controller/HomeController.php
+++ recipes_cor/src/Controller/HomeController.php
@@ -2,72 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\Recipe;
-use App\Form\RecipeType;
-use App\Repository\RecipeRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
 
-    #[Route(path:"/", name:"recipe.index")]
-    public function index(RecipeRepository $repo) : Response {
-        return $this->render("recipe/index.html.twig", ['recipes' => $repo->findAll()]);
-    }
-
-    #[Route('recipe/create', name:'recipe.create')]
-    public function create(Request $request, EntityManagerInterface $em): Response {
-        $recipe = new Recipe();
-        $form = $this->createForm(RecipeType::class, $recipe);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $recipe = $form->getData();
-
-            $em->persist($recipe);
-            $em->flush();
-            $this->addFlash('success','Recipe successfully created !');
-
-            return $this->redirectToRoute('recipe.index');
-        }
-
-        return $this->render('recipe/create.html.twig', ['form' => $form]);
-    }
-
-    #[Route('recipe/edit/{id}', name:'recipe.edit')]
-    public function edit(Recipe $recipe, Request $request, EntityManagerInterface $em) : Response {
-        $form = $this->createForm(RecipeType::class, $recipe);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $recipe = $form->getData();
-
-            $em->persist($recipe);
-            $em->flush();
-            $this->addFlash('success','Recipe successfully edited !');
-
-            return $this->redirectToRoute('recipe.index');
-        }
-
-        return $this->render('recipe/edit.html.twig', ['form' => $form]);
-    }
-
-    #[Route('recipe/delete/{id}', name:'recipe.delete')]
-    public function delete(EntityManagerInterface $em, Recipe $recipe) : Response {
-        if (!$recipe) {
-            $this->addFlash('error','Recipe does not exit !');
-
-            return $this->redirectToRoute('recipe.index');
-        }
-
-        $em->remove( $recipe);
-        $em->flush();
-        $this->addFlash('success','Recipe successfully removed !');
-
-        return $this->redirectToRoute('recipe.index');
+    #[Route(path:"/", name:"home.index")]
+    public function index() : Response {
+        return $this->render("home.html.twig");
     }
 
 }
--- recipes_cor/src/DataFixtures/AppFixtures.php
+++ recipes_cor/src/DataFixtures/AppFixtures.php
@@ -3,8 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\Book;
-use App\Entity\Ingredient;
-use App\Entity\Recipe;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -48,30 +46,7 @@ class AppFixtures extends Fixture {
             $manager->persist($book);
         }
 
-        
-        $ingredients = [];
-        for ($i = 1; $i <= 15; $i++) {
-            $ingredient = new Ingredient();
-            $ingredient->setName("Ingredient " .$i)
-            ->setPrice(mt_rand(0, 20));
-
-            $ingredients[] = $ingredient;
-            $manager->persist($ingredient);
-        }
-
-        for ($i = 1; $i <= 25; $i++) {
-            $recipe = new Recipe();
-            $recipe->setName("Recette ".$i);
-            $recipe->setNbPerson(mt_rand(1,20));
-            $recipe->setDifficulty(mt_rand(1,5));
-            $recipe->setDuration(mt_rand(0,720));
-            $recipe->setDescription("Recipe ".$i. " description");
-
-            for($j = 0; $j < (mt_rand(3, 10)); $j++) {
-                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) -1)]);
-            }
-            $manager->persist($recipe);
-        }
+    
 
         $manager->flush();
     }
